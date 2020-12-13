6b764abdb66bb0ad74da12897bc32fcf0221c769
Ayobamiu <ayobamiu@gmail.com>
2020-12-13 12:26:45 +0100

    Feature: Update Digital Platform

--- src/controllers/digitalPlatforms.controller.js
+++ src/controllers/digitalPlatforms.controller.js
@@ -51,5 +51,39 @@ class DigitalPlatformController {
       return res.status(500).send();
     }
   }
+
+  /**
+   * Update a DigitalPlatform
+   * @param {Request} req - Response object.
+   * @param {Response} res - The payload.
+   * @memberof DigitalPlatformController
+   * @returns {JSON} - A JSON success response.
+   *
+   */
+  static async updateDigitalPlatform(req, res) {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ["name", "link"];
+    const isValidOperation = updates.every((update) =>
+      allowedUpdates.includes(update)
+    );
+    if (!isValidOperation) {
+      return res.status(400).send({ error: "Invalid Updates" });
+    }
+    const digitalPlatform = await DigitalPlatform.findOne({
+      _id: req.params.platformId,
+      artist: req.user._id,
+    });
+    if (!digitalPlatform) {
+      return res.status(404).send({ error: "Not found" });
+    }
+    try {
+      updates.forEach((update) => (digitalPlatform[update] = req.body[update]));
+      await digitalPlatform.save();
+
+      return res.status(200).send(digitalPlatform);
+    } catch (error) {
+      return res.status(400).send(error);
+    }
+  }
 }
 module.exports = DigitalPlatformController;
