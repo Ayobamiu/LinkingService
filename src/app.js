56911ff73da39d4e25db37a13891769d66427a9c
Ayobamiu <ayobamiu@gmail.com>
2020-12-30 07:51:24 +0100

    Feature: Add product to store

--- src/app.js
+++ src/app.js
@@ -1,20 +1,24 @@
 //import
 const express = require("express");
 const userRouter = require("./routers/users.route");
+const productRouter = require("./routers/products.route");
 const artistRoute = require("./routers/artist.route");
 const digitalPlatformRoute = require("./routers/digitalPlatform.route");
 const socialMediaPlatformRoute = require("./routers/socialMedia.route");
 require("./db/mongoose");
 //create express App
+
 const app = express();
+app.use(express.urlencoded());
 app.use(express.json());
 
+app.use("/products", productRouter);
 app.use("/auth", userRouter);
 app.use("/platforms", digitalPlatformRoute);
 app.use("/socials", socialMediaPlatformRoute);
 app.use("/artists", artistRoute);
 
 //tell express to pass incoming request body and send to us in json format
-app.use(express.json());
+// app.use(express.json());
 
 module.exports = app;
---
+++ src/validations/product/addProduct.controller.js
@@ -0,0 +1,66 @@
+const { check, validationResult } = require("express-validator");
+
+/**
+ *Contains AddValidProduct Validator
+ *
+ *
+ *
+ * @class AddValidProduct
+ */
+class AddValidProduct {
+  /**
+   * validate AddValidProduct data.
+   * @memberof AddValidProduct
+   * @returns {null} - No response.
+   */
+  static validateData() {
+    return [
+      check("title")
+        .exists()
+        .withMessage("Title is required")
+        .not()
+        .isEmpty()
+        .withMessage("Title cannot be empty")
+        .isString()
+        .withMessage("Title should be a String"),
+      check("description")
+        .exists()
+        .withMessage("Description is required")
+        .not()
+        .isEmpty()
+        .withMessage("Description cannot be empty")
+        .isString()
+        .withMessage("Description should be a String"),
+      check("cta")
+        .exists()
+        .withMessage("CTA is required")
+        .not()
+        .isEmpty()
+        .withMessage("CTA cannot be empty")
+        .isMongoId()
+        .withMessage("CTA should be a mongoID"),
+    ];
+  }
+
+  /**
+   * Validate results data.
+   * @param {Request} req - Response object.
+   * @param {Response} res - The payload.
+   * @param {Response} next - The next parameter.
+   * @memberof AddValidProduct
+   * @returns {JSON} - A JSON success response.
+   */
+  static async myValidationResult(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const errArr = errors.array().map(({ msg }) => msg);
+      return res.status(400).json({
+        status: "400 Invalid Request",
+        error: "Your request contains invalid parameters",
+        errors: errArr,
+      });
+    }
+    return next();
+  }
+}
+module.exports = AddValidProduct;
