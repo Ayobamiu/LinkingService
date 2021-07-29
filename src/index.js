f0b7d96d6f97728378603f4d8022a3c4f1e17dc2
Ayobamiu <ayobamiu@gmail.com>
2021-07-29 09:59:34 +0100

    Fixing 503 error probably

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
 //App listen to a port
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3003, () => {
   console.log("Server is up and running at Port " + process.env.PORT);
 });
